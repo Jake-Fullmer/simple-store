86fc6b8cb2f6989c96386fb95f77a89a30668ed3
Jake-Fullmer <jnfu@outlook.com>
2020-03-15 13:48:31 -0600

    init

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="main"></div>
+</template>
+
+<script>
+export default {
+  name: "MainComponent",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped>
+</style>
