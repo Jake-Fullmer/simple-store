8bd55ecaacde106901437acd571d4105e5d9a8a3
Jake-Fullmer <jnfu@outlook.com>
2020-03-15 18:16:34 -0600

    variants pics. styling

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,42 +1,65 @@
 <template>
-  <div id="app">
-    <div class="product">
-      <div class="product-image">
-        <img :src="image" />
+  <div class="pt-3" id="main-component">
+    <div class="product container-fluid">
+      <div class="row">
+        <div class="col-12 col-sm-8">
+          <div class="product-image">
+            <img class="image-size" :src="image" />
+            <hr class="hr" />
+            <div class="row justify-content-center">
+              <div v-for="variant in variants" :key="variant.variantId">
+                <p @mouseover="updateProduct(variant.variantImg)">
+                  <img class="image-size-alt" :src="variant.variantImg" />
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-4">
+          <div class="onSale mt-3">
+            <p v-if="onSale" class="on-sale">On Sale Now!</p>
+          </div>
+          <div class="product-info">
+            <h3>{{ product }}</h3>
+            <p v-if="inventory > 10">In Stock</p>
+            <p
+              class="low-supply"
+              v-else-if="inventory <= 10 && inventory > 0"
+            >Order Soon! Only {{inventory}} Remaining</p>
+            <p v-else>Out of Stock</p>
+          </div>
+          <ul>
+            <li v-for="detail in details" :key="detail">{{ detail }}</li>
+          </ul>
+          <button @click="addToCart" class="btn btn-outline-success btn-sm">Add to Cart</button>
+          <button @click="decrementCart" class="btn btn-outline-danger btn-sm ml-1">X</button>
+          <div class="cart">
+            <p>Cart ({{ cart }})</p>
+          </div>
+        </div>
       </div>
-      <div class="product-info">
-        <h1>{{ product }}</h1>
-        <p v-if="inventory > 10">In Stock</p>
-        <p v-else-if="inventory <= 10 && inventory > 0">Order Soon! Only {{inventory}} Remaining</p>
-        <p v-else>Out of Stock</p>
-      </div>
-      <div class="onSale">
-        <p v-if="onSale">On Sale Now!</p>
-      </div>
-      <ul>
-        <li v-for="detail in details" :key="detail">{{ detail }}</li>
-      </ul>
-      <div v-for="variant in variants" :key="variant.variantId">
-        <p @mouseover="updateProduct(variant.variantImg)">{{ variant.variantColor }}</p>
-      </div>
-      <div v-for="size in sizes" :key="size">
-        <p>{{ size }}</p>
-      </div>
-      <button @click="addToCart" class="btn btn-outline-success btn-sm">Add to Cart</button>
-      <div class="cart">
-        <p>Cart ({{ cart }})</p>
+      <div class="row">
+        <div class="col-6">
+          <div v-for="size in sizes" :key="size">
+            <p>{{ size }}</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import black from "../assets/black.jpg";
+import white from "../assets/white.jpg";
+import green from "../assets/green.jpg";
+import blue from "../assets/blue.jpg";
 export default {
   name: "App",
   data() {
     return {
-      product: "Shirt",
-      image: "./assets/stopped-coding-tee-shirt.jpg",
+      product: "Coffee into Code Tee",
+      image: black,
       inventory: 7,
       onSale: true,
       details: ["50% Cotton", "50% Polyester"],
@@ -44,12 +67,22 @@ export default {
         {
           variantId: 6881,
           variantColor: "Black",
-          variantImg: "../assets/black.jpg"
+          variantImg: black
         },
         {
           variantId: 6882,
+          variantColor: "Blue",
+          variantImg: blue
+        },
+        {
+          variantId: 6884,
           variantColor: "White",
-          variantImg: "./assets/white.jpg"
+          variantImg: white
+        },
+        {
+          variantId: 6883,
+          variantColor: "Green",
+          variantImg: green
         }
       ],
       sizes: ["XXL", "XL", "L", "M", "S"],
@@ -59,6 +92,9 @@ export default {
   methods: {
     addToCart() {
       this.cart += 1;
+    },
+    decrementCart() {
+      this.cart -= 1;
     },
     updateProduct(variantImg) {
       this.image = variantImg;
@@ -75,6 +111,29 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+.image-size {
+  height: 55vh;
+  padding: 1rem;
+}
+.image-size-alt {
+  height: 20vh;
+  padding: 1rem;
+}
+.product-image {
+  border: 1px solid gray;
+}
+.on-sale {
+  background-color: red;
+  color: white;
+  padding-top: 0.2rem;
+  padding-bottom: 0.2rem;
+}
+.low-supply {
+  color: red;
+}
+hr.hr {
+  border: 0.2px solid gray;
 }
 </style>
 
--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -15,17 +15,14 @@
       <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">
-            Home
-            <span class="sr-only">(current)</span>
-          </a>
+    <div class="collapse navbar-collapse ml-auto" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+        <li class="nav-item active pl-4">
+          <a class="nav-link" href="#">Tee Shirts</a>
         </li>
       </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+      <form class="form-inline pr-3 ml-auto">
+        <input class="form-control mr-1" type="search" placeholder="Search" aria-label="Search" />
         <button class="btn btn-sm btn-outline-success my-2 my-sm-0" type="submit">Search</button>
       </form>
     </div>
